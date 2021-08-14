35d0252a9f4c2d07bb63b4c1c0c1f13446e24ad8
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-14 23:54:48 +0530

    Create file for section data

---
+++ src/utills/SectionListInitialData.js
@@ -0,0 +1,42 @@
+// util imports
+import {ConstantText} from './ConstantText';
+
+// initial data for section list
+export const SectionListInitialData = [
+  {
+    id: 1,
+    title: ConstantText.incidence,
+    data: [
+      {
+        id: 1,
+        name: ConstantText.days30,
+      },
+      {
+        id: 2,
+        name: ConstantText.days60,
+      },
+      {
+        id: 3,
+        name: ConstantText.days90,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: ConstantText.cases,
+    data: [
+      {
+        id: 1,
+        name: ConstantText.days30,
+      },
+      {
+        id: 2,
+        name: ConstantText.days60,
+      },
+      {
+        id: 3,
+        name: ConstantText.days90,
+      },
+    ],
+  },
+];
