05a9fe9e5dda8ef6898c97228ae9f412ef482542
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-15 00:53:56 +0530

    Add icons for check and uncheck

--- src/assets/Images.js
+++ src/assets/Images.js
@@ -1,3 +1,5 @@
 export const Images = {
   case: require('./img/case.png'),
+  unselectedCheckBox: require('./img/unselectedCheckBox.png'),
+  selectedCheckBox: require('./img/selectedCheckBox.png'),
 };
