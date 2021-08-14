123d8fb1f971b1da52c6a1392a10105c2ec3290c
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-14 23:54:25 +0530

    Create constant file for text's

---
+++ src/utills/ConstantText.js
@@ -0,0 +1,15 @@
+// applicaion text constants
+export const ConstantText = {
+  font_family: 'Avenir',
+  font_family2: 'Avenir Next',
+  font_bold1: '500',
+  font_bold2: '600',
+  font_bold3: '700',
+  network_failure: 'Network request failed',
+  city: 'München',
+  incidence: 'INCIDENCE',
+  cases: 'CASES',
+  days30: '30 days',
+  days60: '60 days',
+  days90: '90 days',
+};
