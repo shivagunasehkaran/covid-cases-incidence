7f6b2292126af139612e3f0acb5d85c66dd69e8e
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-15 01:02:39 +0530

    Cover unit test for updation in Dashboard

--- __tests__/views/Dashboard.test.js
+++ __tests__/views/Dashboard.test.js
@@ -4,6 +4,8 @@ import enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 // page imports
 import Dashboard from '../../src/views/Dashboard';
+// util imports
+import {ConstantText} from '../../src//utills/ConstantText';
 
 enzyme.configure({adapter: new Adapter()});
 
@@ -11,5 +13,50 @@ describe('Dashboard screen', () => {
   it('renders correctly with Dashboard details', async () => {
     const wrapper = shallow(<Dashboard />);
     expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('Dashboard screen : sectionlist', () => {
+  it('should sectionlist return keyExtractor correctly', () => {
+    const DATA = [
+      {
+        id: 1,
+        title: ConstantText.incidence,
+        data: [
+          {
+            id: 1,
+            name: ConstantText.days30,
+          },
+          {
+            id: 2,
+            name: ConstantText.days60,
+          },
+          {
+            id: 3,
+            name: ConstantText.days90,
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: ConstantText.cases,
+        data: [
+          {
+            id: 1,
+            name: ConstantText.days30,
+          },
+          {
+            id: 2,
+            name: ConstantText.days60,
+          },
+          {
+            id: 3,
+            name: ConstantText.days90,
+          },
+        ],
+      },
+    ];
+    const wrapper = shallow(<Dashboard item={DATA} />);
+    expect(wrapper.find('SectionList').length).toEqual(1);
   });
 });
