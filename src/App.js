d06e21ce7947d22c2ab7ba4cdb580af3e36429c3
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-14 17:19:15 +0530

    Move App file into src folder

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>{'Test'}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
