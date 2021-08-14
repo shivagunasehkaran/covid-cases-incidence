83d4e6144f6c7306f7866bbc79dbc05831c4ed86
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-14 23:57:22 +0530

    Create dashboard for showing section list

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,12 +1,72 @@
-// react library imports
-import React from 'react';
-import {View, Text} from 'react-native';
+/**
+ * @format
+ * @flow strict-local
+ */
+// library imports
+import type {Node} from 'react';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  SectionList,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+// util imports
+import {ConstantText} from '../../utills/ConstantText';
+import {SectionListInitialData} from '../../utills/SectionListInitialData';
+// style imports
 import {styles} from './Dashboard.style';
 
-const Dashboard = props => {
+const Dashboard = (props): Node => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // handle index for selection
+  const handleSelectedIndex = id => {
+    if (selectedIndex === id) {
+      setSelectedIndex(-1);
+    } else {
+      setSelectedIndex(id);
+    }
+  };
+
+  // section list child item
+  const Item = ({title, section}) => {
+    return (
+      <View>
+        {selectedIndex === section.id ? (
+          <View style={{backgroundColor: 'green'}}>
+            <Text>{title.name}</Text>
+          </View>
+        ) : null}
+      </View>
+    );
+  };
+
+  // child KeyExtractor
+  const childListKeyExtractor = (item, index) => String(index);
+
   return (
     <View style={styles.container}>
-      <Text>{'Dashboard'}</Text>
+      <SafeAreaView>
+        <Text style={styles.title}>{ConstantText.city}</Text>
+        <View style={styles.listView}>
+          <SectionList
+            sections={SectionListInitialData}
+            renderItem={({item, section}) => (
+              <Item title={item} section={section} />
+            )}
+            keyExtractor={childListKeyExtractor}
+            renderSectionHeader={({section: {title, id}}) => {
+              return (
+                <TouchableOpacity onPress={() => handleSelectedIndex(id)}>
+                  <Text style={styles.header}>{title}</Text>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
--- src/views/Dashboard/Dashboard.style.js
+++ src/views/Dashboard/Dashboard.style.js
@@ -1,11 +1,23 @@
+// react library imports
 import {StyleSheet} from 'react-native';
+// style imports
 import {ColourPalette} from '../../assets/styles/ColourPalette';
+import {ConstantText} from '../../utills/ConstantText';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: ColourPalette.white,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  title: {
+    marginTop: 10,
+    paddingLeft: 17,
+    fontSize: 24,
+    fontFamily: ConstantText.font_family,
+    fontWeight: ConstantText.font_bold1,
+  },
+  listView: {
+    marginVertical: 10,
+    marginHorizontal: 14,
   },
 });
