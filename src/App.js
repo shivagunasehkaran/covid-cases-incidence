e5c2aa72e895eb6836b25fa3c5c0d790f3364756
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-14 17:33:35 +0530

    Fix flow error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow strict-local
  */
 
 import React from 'react';
+import type {Node} from 'react';
 // container component
 import PrimaryContainer from './containers';
 
-const App = () => {
+const App = (): Node => {
   return <PrimaryContainer />;
 };
 
