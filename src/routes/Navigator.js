ac9952ed6b34f64780590d63865740955fab2c20
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-15 22:45:19 +0530

    Setup container for details screen

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,21 +1,47 @@
 // react library imports
 import React from 'react';
+import {Platform} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 // presentational component imports
 import * as ROUTES from './Routes';
 import Dashboard from '../views/Dashboard';
+import Details from '../views/Details';
+import {ConstantText} from '../utills/ConstantText';
+import {ColourPalette} from '../assets/styles/ColourPalette';
 
 const PublicStack = createStackNavigator();
 
 export const PublicRoutes = () => {
   return (
-    <PublicStack.Navigator>
+    <PublicStack.Navigator
+      screenOptions={{
+        headerTintColor: ColourPalette.black,
+        headerBackTitleStyle: {
+          fontSize: 16,
+          fontFamily: ConstantText.font_family,
+          fontWeight: ConstantText.font_bold3,
+        },
+      }}>
       <PublicStack.Screen
         key={ROUTES.pageNameDashoboard}
         name={ROUTES.pageNameDashoboard}
         component={Dashboard}
         options={{
           headerShown: false,
+        }}
+      />
+      <PublicStack.Screen
+        key={ROUTES.pageNameDetails}
+        name={ROUTES.pageNameDetails}
+        component={Details}
+        options={{
+          title: '',
+          headerShown: true,
+          headerBackTitle: ConstantText.back,
+          headerTitle:
+            Platform.OS === ConstantText.platform_android
+              ? ConstantText.back
+              : null,
         }}
       />
     </PublicStack.Navigator>
