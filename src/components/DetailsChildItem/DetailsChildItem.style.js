9e7b697cc25aeb265e3f1597d08c38bfc2ec56d3
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-08-16 10:01:09 +0530

    Updated styles

--- src/components/DetailsChildItem/DetailsChildItem.style.js
+++ src/components/DetailsChildItem/DetailsChildItem.style.js
@@ -8,7 +8,7 @@ import {ConstantText} from '../../utills/ConstantText';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: 5,
+    margin: 10,
     borderRadius: 5,
     paddingBottom: 10,
     backgroundColor: ColourPalette.lightGreen,
